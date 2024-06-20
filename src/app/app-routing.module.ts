2ba5d00bcbeb201d2b3173b96b2579c85e3dddc2
AFOXJONES <alvarofabsas@gmail.com>
2024-06-20 16:37:48 +0200

    loadChildren para enrutamiento child

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { CountriesModule } from './countries/countries.module';
 
 const routes:Routes=[
   {
@@ -19,7 +20,7 @@ const routes:Routes=[
   },
   {
     path:'countries',
-    component: ContactPageComponent
+    loadChildren:()=>import('./countries/countries.module').then(m=>m.CountriesModule)
   },
   {
     path:'**',
