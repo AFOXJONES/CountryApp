71fd1a5d187a583a7404950d4d61d56824500da4
AFOXJONES <alvarofabsas@gmail.com>
2024-08-23 13:50:55 +0200

    Añadiendo el debouncer para buscar

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -10,6 +10,7 @@ import { Country } from '../../interfaces/country';
 export class ByCapitalPageComponent {
 
   public countries: Country[]=[];
+  public isLoading:boolean=false;
 
   constructor(private countriesService:CountriesService){}
 
@@ -17,9 +18,12 @@ export class ByCapitalPageComponent {
     // console.log('Desde By-capital-page')
     // console.log({term})
 
+    this.isLoading=true;
+
     this.countriesService.searchCapital(term)
     .subscribe(countries=>{
       this.countries=countries;
+      this.isLoading=false;
     });
   }
 
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,19 +1,48 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy{
 
+
+  private debouncer:Subject<string>=new Subject<string>();//tipo especial de Observable
+  private debouncerSubscription?:Subscription;
   @Input()
   public placeholder:string='';
 
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncerSubscription=this.debouncer
+    .pipe(
+      debounceTime(800)
+    )
+    .subscribe(value=>{
+      this.onDebounce.emit(value);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe;
+  }
+
+
+
+
+
   emitValue(value:string):void{
     this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm:string){
+    this.debouncer.next(searchTerm);
   }
 }
