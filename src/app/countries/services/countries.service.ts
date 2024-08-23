bd7587f91d8ceb74d28de11db6c45ffd49ff5f19
AFOXJONES <alvarofabsas@gmail.com>
2024-08-23 14:17:41 +0200

    Manteniendo los datos en las paginas

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, tap, of, delay } from 'rxjs';
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,13 @@ import { Country } from '../interfaces/country';
 export class CountriesService {
 
 
-  private apiUrl:string = 'https://restcountries.com/v3.1'
+  private apiUrl:string = 'https://restcountries.com/v3.1';
+
+  public cacheStore:CacheStore={
+    byCapital:{term:'',countries:[]},
+    byCountries:{term:'',countries:[]},
+    byRegion:{region:'',countries:[]}
+  }
 
   constructor(private http: HttpClient) { }
 
